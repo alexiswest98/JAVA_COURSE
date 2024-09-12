bdabd902076965abd64333f4c14e9b043e574f5c
Alexis West <alexishwest98@gmail.com>
2024-09-11 17:55:29 -0700

    added lab 6

---
+++ CalulatingLoanFee.java
@@ -0,0 +1,43 @@
+//Alexis West
+//September 11, 2024
+//Lab 6
+// This program will calculate and print a service charge for cashing a check using an if - else statement.
+
+import java.util.Scanner;
+
+public class CalulatingLoanFee
+{
+    public static void main(String[] args) 
+    {
+        //store amount of check from user 
+        Scanner myCheckScanner = new Scanner(System.in);
+
+        System.out.println("What is the value of your check?");
+
+        double checkAmount = myCheckScanner.nextDouble();
+        double serviceCharge = 0;
+
+        // if check < 10; charge +1
+        if(checkAmount < 10)
+            serviceCharge++;
+        //if check > 10 but < 100; charge x .10 of og amount
+        else if (checkAmount > 10 && checkAmount < 100) 
+        {
+            serviceCharge = checkAmount * .10;
+        }
+        //if amount > 100 but < 1000; charge + 5 + .05 of og amount
+        else if (checkAmount > 100 && checkAmount < 1000) 
+        {
+            serviceCharge = 5 + (checkAmount * .05);
+        }
+        //if value > 1000; charge + 40 + .01 of og amount
+        else 
+            serviceCharge = 40 + (checkAmount * .01);
+        
+        double returnedAmount = checkAmount - serviceCharge;
+
+        System.out.println((int)checkAmount + "(amount of check)\n" +
+                            (int)serviceCharge + "(service charge)\n" +
+                            (int)returnedAmount + "(amount returned to you)");
+    }
+}
--- Ch3/Ch3_Notes.md
+++ Ch3/Ch3_Notes.md
@@ -27,3 +27,42 @@ else if (s1.compareTo(s2) > 0)
 else//s1.compareTo(s2) == 0
    System.out.println(s1 + " equals " + s2);
 ```
+## enumerated types
+**EXAMPLE**
+```
+// Defining an enum for days of the week
+public enum Day {
+    SUNDAY,
+    MONDAY,
+    TUESDAY,
+    WEDNESDAY,
+    THURSDAY,
+    FRIDAY,
+    SATURDAY
+}
+
+// Using the enum in a class
+public class EnumExample {
+    public static void main(String[] args) {
+        // Assigning an enum value to a variable
+        Day today = Day.WEDNESDAY;
+
+        // Using the enum in a switch statement
+        switch (today) {
+            case MONDAY:
+                System.out.println("Start of the work week!");
+                break;
+            case WEDNESDAY:
+                System.out.println("Midweek already!");
+                break;
+            case FRIDAY:
+                System.out.println("Almost the weekend!");
+                break;
+            default:
+                System.out.println("Another day of the week.");
+                break;
+        }
+    }
+}
+
+```
